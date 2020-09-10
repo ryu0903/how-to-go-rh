ded337f0c9e2c293e49609152beffa5e52bb89da
ryu0903 <ryu0903.higashi.2@gmail.com>
2020-09-10 15:02:02 +0000

    make data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  require "uri"
+  
   def full_title(page_title = '')
     base_title = 'How to Go'
     if page_title.blank?
@@ -6,5 +8,16 @@ module ApplicationHelper
     else
       "#{page_title} - #{base_title}"
     end
+  end
+  
+  def text_url_to_link(text)
+    URI.extract(text, ['http', "https"]).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+  
+    return text
   end
 end
