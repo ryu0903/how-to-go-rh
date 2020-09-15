93c2fd3df685d9a0a84db6defcc30ead7da43716
ryu0903 <ryu0903.higashi.2@gmail.com>
2020-09-15 12:11:12 +0000

    login_for_recruiter

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,7 +4,13 @@ class ToppagesController < ApplicationController
       @destinations = current_user.feed_destination.order(id: :desc).page(params[:page]).per(5)
       @title = "Everyone's Posts"
     else
-      @title = "What's How to Go?"
+      @title = "Login for recruiter"
     end
   end
+  
+  def about
+    @title = "What's How to Go?"
+  end
+  
 end
+
